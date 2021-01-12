3ac772e5f433645f50cdc0af6115cc8b55fea327
Darshan <darshankumarzalavadiya154@gmail.com>
2021-01-12 10:49:09 +0530

    redux for products

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+import productRoutes from './routes/productRoute.js';
 
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
+app.use(notFound);
 
-  res.json(product);
-});
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running on port ${PORT}`));
+app.listen(PORT, console.log(`server running on port ${PORT}`.yellow.bold));
